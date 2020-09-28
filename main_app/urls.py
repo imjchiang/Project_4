bdac69d81ba040e2b6871cb9cac5c9a38e2c92ec
imjchiang <jchiang296@gmail.com>
2020-09-28 03:03:06 -0700

    [urls.py] add paths for login, logout, and signup [views.py] render auth stuff

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("", views.index, name = "index"),
     path("profile/r/", views.rider_profile, name = "rider_profile"),
     path("profile/d/", views.driver_profile, name = "driver_profile"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("signup/", views.signup_view, name="signup"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,6 +8,43 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.models import User
 
 
+def login_view(request):
+    if request.method == "POST":
+        # try to log the user in
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            u = form.cleaned_data["username"]
+            p = form.cleaned_data["password"]
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user) # login user by creating a session
+                    return HttpResponseRedirect("/user/" + u)
+                else:
+                    print("The account has been disabled.")
+            else:
+                print("The username and/or password is incorrect.")
+    else:
+        form = AuthenticationForm()
+        return render(request, "login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect("/cats")
+
+def signup_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return HttpResponseRedirect("/user/" + str(user))
+        else:
+            return HttpResponse("<h1>Try Again</h1>")
+    else:
+        form = UserCreationForm()
+        return render(request, "signup.html", {"form": form})
+
 ####################### RIDER #######################
 
 # profile page for riders
