48e0000a99af7d35fd073f178535953d4557c759
imjchiang <jchiang296@gmail.com>
2020-09-28 12:57:55 -0700

    [settings.py] set up mongo database (unsure if it works)

--- Project_4/Project_4/settings.py
+++ Project_4/Project_4/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Project_4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': "ride_or_share",
+        "USER": "imjchiang",
+        "PASSWORD": 5375,
+        "HOST": "mongodb://localhost:27017/",
+        "PORT": 8000
     }
 }
 
