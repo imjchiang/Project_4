322614094c1cab7e64f5bf3ca49a2fc29a1bb73f
imjchiang <jchiang296@gmail.com>
2020-09-28 02:49:55 -0700

    [main_app/urls.py] add paths for profile [main_app/views.py] add rendering for profile

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name = "index")]
+# set paths for pages here
+urlpatterns = [
+    path("", views.index, name = "index"),
+    path("profile/r/", views.rider_profile, name = "rider_profile"),
+    path("profile/d/", views.driver_profile, name = "driver_profile"),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.models import User
 
+
+####################### RIDER #######################
+
+# profile page for riders
+def rider_profile(request):
+    return render(request, "riders/profile.html")
+
+####################### DRIVER #######################
+
+# profile page for drivers
+def driver_profile(request):
+    return render(request, "drivers/profile.html")
+
+####################### DEFAULT #######################
+
+# home page
 def index(request):
     return render(request, "index.html")
