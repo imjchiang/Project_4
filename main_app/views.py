626cd3b0ada969dd1e7cdb9d3ed09b4ed79da44e
imjchiang <jchiang296@gmail.com>
2020-09-29 17:15:11 -0700

    [views.py] implement email and name in form, unique email

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,41 @@
-from django.shortcuts import render, redirect
+from django import forms
+
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from django.utils.decorators import method_decorator
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
+
+from django.core.exceptions import ValidationError
+
+from .models import Rider, Driver
 
 
 ####################### USER #######################
+class MyUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label='Email')
+
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+
+        if User.objects.filter(email=user.email).exists():
+            raise ValidationError("Email has already been used. Please use a different one.")
+        
+        if commit:
+            user.save()
+        return user
 
 def login_view(request):
     if request.method == "POST":
@@ -36,8 +63,7 @@ def logout_view(request):
 
 def signup_view(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        print("HELLO HERE", form, "BYE THERE")
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
@@ -45,12 +71,12 @@ def signup_view(request):
         else:
             return HttpResponse("<h1>Try Again</h1>")
     else:
-        form = UserCreationForm()
+        form = MyUserCreationForm()
         return render(request, "signup.html", {"form": form})
 
 @login_required
 def profile(request, username):
-    print(User)
+    print(User.username)
     return render(request, "profile.html", {"username": username})
 
 
@@ -60,6 +86,11 @@ def profile(request, username):
 @login_required
 def rider_profile(request, username):
     return render(request, "riders/profile.html", {"username": username})
+
+class RiderCreate(CreateView):
+    model = Rider
+    fields = "__all__"
+    success_url = "/profile/{}".format(User.username)
 
 ####################### DRIVER #######################
 
