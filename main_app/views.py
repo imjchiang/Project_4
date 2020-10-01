36702e9b936a9d6f7056a1d6ee9c814d1bd70674
imjchiang <jchiang296@gmail.com>
2020-09-30 18:00:34 -0700

    [profile.html] add buttons for form and sub-profiles [views.py] send conditions for buttons to profile

--- main_app/views.py
+++ main_app/views.py
@@ -82,10 +82,21 @@ def signup_view(request):
 
 @login_required
 def profile(request, username):
-    # if Rider for the user exists, pass rider info.
-    # otherwise send rider form to profile page
-    # same thing for driver
-    return render(request, "profile.html", {"username": username})
+    user = User.objects.get(username = username)
+    # if Rider for the user exists, pass parameter to rider profile redirect
+    if Rider.objects.filter(user_key_id = user.id).exists():
+        rider_register = True
+    # otherwise profile page redirect to rider form
+    else: 
+        rider_register = False
+    
+    # if Driver for the user exists, pass parameter to driver profile redirect
+    if Driver.objects.filter(user_key_id = user.id).exists():
+        driver_register = True
+    # otherwise profile page redirect to driver form
+    else: 
+        driver_register = False
+    return render(request, "profile.html", {"username": username, "rider_register": rider_register, "driver_register": driver_register})
 
 
 ####################### RIDER #######################
@@ -109,13 +120,13 @@ class RiderCreate(CreateView):
 
         # ensures there are no riders associated with the account
         if Rider.objects.filter(user_key_id = user.id).exists():
-            # don't register rider if there is alreadyy one registered
+            # don't register rider if there is already one registered
             raise ValidationError("You cannot create another Rider in your account.")
         # register a new rider and redirect to profile page of rider
         else:
             rider.save()
             return HttpResponseRedirect("/profile/{}/r".format(self.request.user.username))
-            # return super(RiderCreate, self).form_valid(form)
+            # [possible alternative] return super(RiderCreate, self).form_valid(form)
         
 
 ####################### DRIVER #######################
@@ -124,6 +135,41 @@ class RiderCreate(CreateView):
 @login_required
 def driver_profile(request, username):
     return render(request, "drivers/profile.html", {"username": username})
+
+# create a new rider
+class DriverCreate(CreateView):
+    model = Driver
+    fields = ["trip_distance", "rate", "rush_hour_rate", "vehicle_type", "vehicle_make", "vehicle_model", "vehicle_year", "vehicle_insured", "license_expiration"]
+
+    def get_form(self, form_class = None):
+        form = super(DriverCreate, self).get_form(form_class)
+        form.fields["trip_distance"].required = True
+        form.fields["rate"].required = True
+        form.fields["rush_hour_rate"].required = True
+        form.fields["vehicle_type"].required = True
+        form.fields["vehicle_make"].required = True
+        form.fields["vehicle_model"].required = True
+        form.fields["vehicle_year"].required = True
+        form.fields["vehicle_insured"].required = True
+        form.fields["license_expiration"].required = True
+        
+        return form
+
+    def form_valid(self, form):
+        # get User object of the logged in account
+        user = User.objects.get(pk = self.request.user.id)
+        driver = form.save(commit = False)
+        # set user_key to the user object grabbed
+        driver.user_key = user
+
+        # ensures there are no drivers associated with the account
+        if Driver.objects.filter(user_key_id = user.id).exists():
+            # don't register driver if there is already one registered
+            raise ValidationError("You cannot create another Driver in your account.")
+        # register a new driver and redirect to profile page of rider
+        else:
+            driver.save()
+            return HttpResponseRedirect("/profile/{}/d".format(self.request.user.username))
 
 ####################### DEFAULT #######################
 
