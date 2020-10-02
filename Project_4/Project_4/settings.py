3c803ccdd84b81ee69f1ea3fd4aa13ba58c5d971
imjchiang <jchiang296@gmail.com>
2020-10-02 02:53:31 -0700

    [settings.py] add STATIC_URL and STATICFILES_DIRS [urls.py] import necessary static stuff, implement static stuff [style.css] test styling

--- Project_4/Project_4/settings.py
+++ Project_4/Project_4/settings.py
@@ -158,6 +158,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+ROOT_PATH = os.path.dirname(__file__)
+STATICFILES_DIRS = [os.path.join(ROOT_PATH, "static")]
+
 # # Define EMAIL_BACKEND variable for DJANGO_HOST
 # if DJANGO_HOST == "production":
 #     # Output to SMTP server on DJANGO_HOST production
